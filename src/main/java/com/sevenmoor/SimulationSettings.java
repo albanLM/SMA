28bc0fb2fe0aa2d5425c6a8ffb69bf1016f530bf
SevenMoor <au.ottaviano@yahoo.com>
2020-12-13 21:35:12 +0100

    Doc: SimulationSettings.java

--- src/main/java/com/sevenmoor/SimulationSettings.java
+++ src/main/java/com/sevenmoor/SimulationSettings.java
@@ -2,22 +2,22 @@ package com.sevenmoor;
 
 /** Settings to be used for the simulation. */
 public class SimulationSettings {
-    /** Number of com.sevenmoor.agents to be created for the simulation. */
+    /** Number of ProducerConsumerAgent instances to be created for the simulation. */
     public final int agentCount;
+
     /** Names of the products to be created for the simulation. */
     public final String[] productNames;
-    /** How much money the agent will have at the start of the simulation */
+
+    /** How much money each agent will have at the start of the simulation */
     public final float startMoney;
+
     /** How much time the simulation will run in seconds. */
     public final long simulationDuration;
 
-    public SimulationSettings(int agentCount, String[] productNames, long simulationDuration, float startMoney) {
-        this.agentCount = agentCount;
-        this.productNames = productNames;
-        this.startMoney = startMoney;
-        this.simulationDuration = simulationDuration;
-    }
-
+    /**
+     * Uses the Arguments passed to the simulation to create human-readable settings
+     * @param args an Object array that represent the parameters
+     */
     public SimulationSettings(Object[] args){
         agentCount = Integer.parseInt((String) args[0]);
         startMoney = Float.parseFloat((String) args[1]);
